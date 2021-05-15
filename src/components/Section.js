565cae6209d5fa51a5350fcc8e584c9e5555143a
Sven Notermans <sven.notermans@live.nl>
2021-05-15 17:09:14 +0200

    started with section

---
+++ src/components/Section.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Section = () => {
+  return (
+    <Wrap>
+      
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background: url("/images/model-s.jpg");
+`
