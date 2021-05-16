dec72b51afb89cd2914086f6092108db5699d8a4
Sven Notermans <sven.notermans@live.nl>
2021-05-16 14:52:48 +0200

    add redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import styled from "styled-components"
+import Fade from 'react-reveal/Fade';
 
 const Section = ({title, description, backgroundImg, leftBtnText, rightBtnText}) => {
   return (
     <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
       <ItemText>
         <h1>{title}</h1>
         <p>{description}</p>
       </ItemText>
+      </Fade>
       <Buttons>
+        <Fade bottom>
       <ButtonGroup>
         <LeftButton>
           {leftBtnText}
@@ -19,6 +23,7 @@ const Section = ({title, description, backgroundImg, leftBtnText, rightBtnText})
         </RightButton>
         }
       </ButtonGroup>
+      </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
